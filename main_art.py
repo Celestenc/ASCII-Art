01933707c35563aee2acb8f9ee2428a089c6e108
Celestenc <celestencarrasco@gmail.com>
2024-01-10 17:40:54 -0500

    Fully working ASCII Image Art

--- main_art.py
+++ main_art.py
@@ -2,31 +2,31 @@ from PIL import Image
 import numpy as np
 
 ## 1) Uploading image to work with
-im = Image.open("ascii-pineapple.jpg")
-# print("Successfully loaded image!\n" + "Image size: " + str(im.size[0]) +  " x " + str(im.size[1]))
+ima = Image.open("ascii-pineapple.jpg")
+im = ima.resize((round(ima.size[0]*0.5), round(ima.size[1]*0.5)))
+print("Successfully loaded image!\n" + "Image size: " + str(im.size[0]) +  " x " + str(im.size[1]))
 
 
 ## 4) Converting image's pixel data into an ASCII color-array + printing
-
 pixel_array = np.asarray(im)
-
 bright_arr = []
-key = 255/65
 ascii_str = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 ascii_str_length = len(ascii_str)
-print("Length of ascii_str:", ascii_str_length) 
-for i in range(len(pixel_array)):
-    row = []
-    for j in range(len(pixel_array[i])):
-        avg = (sum(pixel_array[i][j]))/3
+key = 255/(ascii_str_length - 1)
+
+print("Length of ascii_str:", ascii_str_length)
+for x in range(len(pixel_array)):
+    row_array = []
+    for y in range(len(pixel_array[x])):
+        avg = (sum(pixel_array[x][y]))/3
         index = round(avg/key)
-        index = min(index, 64)
-        row.append(ascii_str[index])
-    bright_arr.append(row)
+        index = min(index, (ascii_str_length - 1))
+        row_array.append(ascii_str[index] * 3) 
+    bright_arr.append(row_array)
+ 
 
-print("Iterating through pixel contents:")
-
-# Printing just the tuples from tuple array
+## 5 Printing the ASCII Image!
 for i in range(len(bright_arr)):
     for j in range(len(bright_arr[i])):
-        print(bright_arr[i][j])
+        print(bright_arr[i][j], end = "")
+    print("")
