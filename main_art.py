902018c9ac2a4e87d9a32441ca5be3e707ddd606
Celestenc <celestencarrasco@gmail.com>
2024-01-09 20:01:44 -0500

    Part III Converting RGB values into single brightness numbers

--- main_art.py
+++ main_art.py
@@ -16,17 +16,33 @@ pixel_array = np.asarray(im)
 #     for j in range(len(pixel_array[i])):
         # print(pixel_array[i][j])
 
-# Creating and printing a 2D array of tuples
-tup_array = []
+# # Creating and printing a 2D array of tuples
+# tup_array = []
+# for i in range(len(pixel_array)):
+#     row = []
+#     for j in range(len(pixel_array[i])):
+#         row.append(tuple(pixel_array[i][j]))
+#     tup_array.append(row)
+
+# print("Iterating through pixel contents:")
+
+# # Printing just the tuples from tuple array
+# for i in range(1):
+#     for j in range(len(tup_array[i])):
+#         print(tup_array[i][j])
+
+## 3) Converting RGB matrix into a Brightness matrix
+# Creating and printing a 2D array of average of colors
+bright_arr = []
 for i in range(len(pixel_array)):
     row = []
     for j in range(len(pixel_array[i])):
-        row.append(tuple(pixel_array[i][j]))
-    tup_array.append(row)
+        row.append((sum(pixel_array[i][j]))//3)
+    bright_arr.append(row)
 
 print("Iterating through pixel contents:")
 
 # Printing just the tuples from tuple array
-for i in range(1):
-    for j in range(len(tup_array[i])):
-        print(tup_array[i][j])
+for i in range(len(bright_arr)):
+    for j in range(len(bright_arr[i])):
+        print(bright_arr[i][j])
